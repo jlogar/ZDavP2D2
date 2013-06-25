dc194d3726ee998ea43a424c203689b72b12452d
Jernej Logar <jernej.logar@gmail.com>
2013-06-25 16:27:45 +0200

    link to source

--- ZDavP2D2/InvoiceAggregateRecord.cs
+++ ZDavP2D2/InvoiceAggregateRecord.cs
@@ -2,6 +2,9 @@
 
 namespace ZDavP2D2
 {
+    /// <summary>
+    /// "Zbirni podatki" as defined at 2.1 of http://www.uradni-list.si/files/RS_-2013-035-01312-OB~P001-0000.PDF#!/pdf
+    /// </summary>
     public class InvoiceAggregateRecord
     {
         public string DavSt { get; set; }
--- ZDavP2D2/InvoiceItemRecord.cs
+++ ZDavP2D2/InvoiceItemRecord.cs
@@ -1,5 +1,8 @@
 ﻿namespace ZDavP2D2
 {
+    /// <summary>
+    /// "Podatki o postavkah" as defined at 2.2 of http://www.uradni-list.si/files/RS_-2013-035-01312-OB~P001-0000.PDF#!/pdf
+    /// </summary>
     public class InvoiceItemRecord
     {
     }
